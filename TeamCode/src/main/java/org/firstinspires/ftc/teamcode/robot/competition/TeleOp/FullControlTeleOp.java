4c1552ceec9027b241edb6465c41853e15525d7b
student <ftcrobotics@mtbethelchristian.org>
2018-09-21 16:59:05 -0400

    Created Full Control Teleop Java Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/competition/TeleOp/FullControlTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/competition/TeleOp/FullControlTeleOp.java
@@ -1,4 +1,157 @@
 package org.firstinspires.ftc.teamcode.robot.competition.TeleOp;
 
-public class FullControlTeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.robot.old.classes.sub.boardArm;
+import org.firstinspires.ftc.teamcode.robot.old.classes.sub.colorSensorArm;
+import org.firstinspires.ftc.teamcode.robot.old.classes.sub.glyphArms;
+import org.firstinspires.ftc.teamcode.robot.old.classes.sub.glyphLift;
+import org.firstinspires.ftc.teamcode.robot.old.classes.sub.relicArm;
+
+/**
+ * Created by blake_shafer on 8/23/17.
+ */
+
+@Disabled
+@TeleOp(name = "Full Control (OLD RELIC RECOVERY)")
+
+public class FullControlTeleOp extends OpMode {
+
+    // left stick y axis controls forward/backward rotation of left motors
+    // right stick y axis controls forward/backward rotation of right motors (tank drive)
+    // left/right triggers control strafing left/right
+
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor rearLeftMotor;
+    DcMotor rearRightMotor;
+
+    double leftStickVal;
+    double rightStickVal;
+
+    double leftTriggerVal;
+    double rightTriggerVal;
+
+    double frontLeftSpeed;
+    double frontRightSpeed;
+    double rearLeftSpeed;
+    double rearRightSpeed;
+
+    boolean reverseMode;
+
+    boolean initServos;
+
+
+
+    @Override
+    public void init() {
+
+        frontLeftMotor = hardwareMap.dcMotor.get("front_left_motor");
+        frontRightMotor = hardwareMap.dcMotor.get("front_right_motor");
+        rearLeftMotor = hardwareMap.dcMotor.get("rear_left_motor");
+        rearRightMotor = hardwareMap.dcMotor.get("rear_right_motor");
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        reverseMode = false;
+
+        initServos = false;
+    }
+    @Override
+    public void loop() {
+
+
+        if (!initServos) {
+            initServos = true;
+        }
+
+        if (gamepad1.dpad_up) {
+            reverseMode = false;
+        }
+
+        else if (gamepad1.dpad_down) {
+            reverseMode = true;
+        }
+
+        leftStickVal = -gamepad1.left_stick_y;
+        leftStickVal = Range.clip(leftStickVal, -1, 1);
+        rightStickVal = -gamepad1.right_stick_y;
+        rightStickVal = Range.clip(rightStickVal, -1, 1);
+
+        leftTriggerVal = gamepad1.left_trigger;
+        leftTriggerVal = Range.clip(leftTriggerVal, 0, 1);
+        rightTriggerVal = gamepad1.right_trigger;
+        rightTriggerVal = Range.clip(rightTriggerVal, 0, 1);
+
+        if (reverseMode) {
+            frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+            rearLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+            frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+            rearRightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+            frontLeftSpeed = leftStickVal - leftTriggerVal + rightTriggerVal;
+            frontLeftSpeed = Range.clip(frontLeftSpeed, -1, 1);
+
+            frontRightSpeed = rightStickVal - rightTriggerVal + leftTriggerVal;
+            frontRightSpeed = Range.clip(frontRightSpeed, -1, 1);
+
+            rearLeftSpeed = leftStickVal + leftTriggerVal - rightTriggerVal;
+            rearLeftSpeed = Range.clip(rearLeftSpeed, -1, 1);
+
+            rearRightSpeed = rightStickVal + rightTriggerVal - leftTriggerVal;
+            rearRightSpeed = Range.clip(rearRightSpeed, -1, 1);
+
+            frontLeftMotor.setPower(rearRightSpeed);
+            frontRightMotor.setPower(rearLeftSpeed);
+            rearLeftMotor.setPower(frontRightSpeed);
+            rearRightMotor.setPower(frontLeftSpeed);
+
+        }
+
+        else {
+            frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+            rearLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+            frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+            rearRightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+            frontLeftSpeed = leftStickVal - leftTriggerVal + rightTriggerVal;
+            frontLeftSpeed = Range.clip(frontLeftSpeed, -1, 1);
+
+            frontRightSpeed = rightStickVal - rightTriggerVal + leftTriggerVal;
+            frontRightSpeed = Range.clip(frontRightSpeed, -1, 1);
+
+            rearLeftSpeed = leftStickVal + leftTriggerVal - rightTriggerVal;
+            rearLeftSpeed = Range.clip(rearLeftSpeed, -1, 1);
+
+            rearRightSpeed = rightStickVal + rightTriggerVal - leftTriggerVal;
+            rearRightSpeed = Range.clip(rearRightSpeed, -1, 1);
+
+            frontLeftMotor.setPower(frontLeftSpeed);
+            frontRightMotor.setPower(frontRightSpeed);
+            rearLeftMotor.setPower(rearLeftSpeed);
+            rearRightMotor.setPower(rearRightSpeed);
+        }
+
+
+        // Telemetry
+
+        //telemetry.addData("val", "L stck: " + leftStickVal);
+        //telemetry.addData("val", "R stck: " + rightStickVal);
+        //telemetry.addData("val", "L trgr: " + leftTriggerVal);
+        //telemetry.addData("val", "R trgr: " + rightTriggerVal);
+
+        //telemetry.addData("pwr", "FL mtr: " + frontLeftSpeed);
+        //telemetry.addData("pwr", "FR mtr: " + frontRightSpeed);
+        //telemetry.addData("pwr", "RL mtr: " + rearLeftSpeed);
+        //telemetry.addData("pwr", "RR mtr: " + rearRightSpeed);
+
+        telemetry.update();
+    }
 }
