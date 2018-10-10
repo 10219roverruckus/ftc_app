8e2d8e97e60f3e1508029c4680527487eea5e1e4
johnduval <john.duval@mtbethelchristian.org>
2018-10-10 13:54:11 -0400

    Updated outreach robot.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/outreach/OutreachRobot.java
@@ -0,0 +1,148 @@
+package org.firstinspires.ftc.teamcode.robot.outreach;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.LED;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.robot.outreach.mechanisms.motors.CatapultArm;
+
+import static java.lang.Thread.sleep;
+
+
+@TeleOp(name = "Outreach Robot")
+public class OutreachRobot extends OpMode {
+
+    outreachMotors myOutreachMotors;
+    //outreachCatapultMotorsRunToPosition myOutreachCatapultMotorsRunToPosition;
+    //outreachCatapultMotorsRunUsingEncoders myOutreachCatapultMotorsRunUsingEncoders;
+    //outreachCatapultMotorsRunWithoutEncoders myOutreachCatapultMotorsRunWithoutEncoders;
+    //outreachCatapultMotorsRunUsingEncoders myOutreachCatapultMotorsRunUsingEncoders;
+    //outreachCatapultMotorsRunToPosition2motors myOutreachCatapultMotorsRunToPosition2motors;
+
+    CatapultArm myCatapultArm;
+
+    //outreachCatapultMotorsRunWithoutEncoders2motors myOutreachCatapultMotorsRunWithoutEncoders2motors;
+
+    outreachTouchSensorCatapult myOutreachTouchSensorCatapult;
+    //value for left joystick
+    double leftY;
+    //value for right joystick
+    double rightY;
+
+    double triggerLeft;
+    double triggerRight;
+    DcMotor catapult;
+
+    double minLedPosition = 0.2525;
+    double maxLedPosition = 0.7475;
+    double ledPosition = minLedPosition;
+
+    Servo ledStrip;
+
+    boolean bumperAllow = true;
+
+    @Override
+    public void init() {
+        myOutreachMotors = new outreachMotors(hardwareMap.dcMotor.get("left_drive_motor"), hardwareMap.dcMotor.get("right_drive_motor"));
+        myCatapultArm = new CatapultArm(hardwareMap.dcMotor.get("left_catapult_motor"), hardwareMap.dcMotor.get("right_catapult_motor"));
+        myOutreachTouchSensorCatapult = new outreachTouchSensorCatapult (hardwareMap.get(DigitalChannel.class, "catapult_touch_sensor"));
+
+        ledStrip = hardwareMap.servo.get("led_strip");
+        ledStrip.setPosition(ledPosition);
+
+
+
+    }
+
+    @Override
+    public void loop() {
+
+        if (gamepad1.left_trigger > 0 || gamepad1.right_trigger > 0) {
+            ledPosition = ledPosition - (gamepad1.left_trigger/1000);
+            ledPosition = ledPosition + (gamepad1.right_trigger/1000);
+            ledPosition = Range.clip(ledPosition,minLedPosition,maxLedPosition);
+        }
+
+        if (gamepad1.left_bumper) {
+            ledPosition = minLedPosition;
+        }
+        if (gamepad1.right_bumper) {
+            ledPosition = maxLedPosition;
+        }
+
+//        if (bumperAllow == true && (gamepad1.left_bumper == true || gamepad1.right_bumper == true)) {
+//            if (gamepad1.left_bumper) {
+//                ledPosition = ledPosition - .1;
+//                ledPosition = Range.clip(ledPosition,minLedPosition,maxLedPosition);
+//                ledStrip.setPosition(ledPosition);
+//            }
+//            if (gamepad1.right_bumper) {
+//                ledPosition = ledPosition + .1;
+//                ledPosition = Range.clip(ledPosition,minLedPosition,maxLedPosition);
+//                ledStrip.setPosition(ledPosition);
+//            }
+//            bumperAllow = false;
+//        }
+//        else if (!bumperAllow && !gamepad1.left_bumper && !gamepad1.right_bumper) {
+//            bumperAllow = true;
+//        }
+
+        leftY = gamepad1.left_stick_y;
+        rightY = gamepad1.right_stick_y;
+        myOutreachMotors.drive(leftY, rightY);
+        //trigger left lowers arm
+        //trigger right raises arm
+//        triggerLeft = gamepad1.left_trigger;
+//        triggerRight = gamepad1.right_trigger;
+//        myCatapultArm.catapultMotorManualOperation(triggerLeft, triggerRight, myOutreachTouchSensorCatapult);
+
+
+
+
+        if (gamepad1.y) {
+            myCatapultArm.catapultReset(myOutreachTouchSensorCatapult);
+            myCatapultArm.motorPowerZero();
+            ledPosition = .43;
+            ledStrip.setPosition(ledPosition);  //needed because will never get to the IF statement at bottom of loop()
+            myCatapultArm.catapultLaunch(myOutreachTouchSensorCatapult);
+            myCatapultArm.motorPowerZero();
+            myCatapultArm.catapultReset(myOutreachTouchSensorCatapult);
+            ledPosition = .4;
+            myCatapultArm.motorPowerZero();
+        }
+
+        if (gamepad1.a) {
+            myCatapultArm.catapultReset(myOutreachTouchSensorCatapult);
+            myCatapultArm.motorPowerZero();
+        }
+
+        if (gamepad1.x) {
+            myCatapultArm.catapultMotorManualOperation(.3, 0, myOutreachTouchSensorCatapult);
+            myCatapultArm.motorPowerZero();
+        }
+
+        if (gamepad1.b) {
+            myCatapultArm.catapultMotorManualOperation(0, .6, myOutreachTouchSensorCatapult);
+            myCatapultArm.motorPowerZero();
+        }
+
+        if (ledPosition != ledStrip.getPosition()) {
+            ledStrip.setPosition(ledPosition);
+        }
+
+        //catapult.setPower(-triggerLeft);
+        //catapult.setPower(triggerRight);
+        telemetry.addData("Touch Sensor: ", myOutreachTouchSensorCatapult.checkCatapultTouchSensor());
+        telemetry.addData("LED Pos VAR: ", ledPosition);
+        telemetry.addData("LED getPosition: ", ledStrip.getPosition());
+        telemetry.addData("Left Y: ", leftY);
+        telemetry.addData("Right Y: ", rightY);
+        telemetry.addData("Left trigger: ", gamepad1.left_trigger);
+        telemetry.addData("Right Trigger: ", gamepad1.right_trigger);
+        telemetry.update();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/outreach/mechanisms/motors/CatapultArm.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode.robot.outreach.mechanisms.motors;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.robot.outreach.outreachTouchSensorCatapult;
+
+public class CatapultArm {
+    public DcMotor leftMotor;
+    public DcMotor rightMotor;
+    public final DcMotor.RunMode currentMotorRunMode = DcMotor.RunMode.RUN_WITHOUT_ENCODER;
+    public final DcMotor.ZeroPowerBehavior currentMotorBrakeMode = DcMotor.ZeroPowerBehavior.BRAKE;
+    public static final int TICKS_PER_ROTATION = 1680; // TICKS PER ROTATION NEEDED
+    public static final int LAUNCH_DISTANCE = 195; //distance arms will travel.
+    public static final double LAUNCH_POWER = 1;
+    public static final double RESET_POWER = -.2;
+
+    public CatapultArm (DcMotor LM, DcMotor RM) {
+        leftMotor = LM;
+        rightMotor = RM;
+
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        setMotorRunModes(currentMotorRunMode);
+        setCurrentMotorBrakeMode(currentMotorBrakeMode);
+    }
+
+    public void setMotorRunModes (DcMotor.RunMode mode) {
+        leftMotor.setMode(mode);
+        rightMotor.setMode(mode);
+    }
+
+    public void setCurrentMotorBrakeMode (DcMotor.ZeroPowerBehavior zero) {
+        leftMotor.setZeroPowerBehavior(zero);
+        rightMotor.setZeroPowerBehavior(zero);
+    }
+
+    public void catapultMotorManualOperation (double armLower, double armRaise, outreachTouchSensorCatapult touchSensor) {
+        if (armLower > 0 && touchSensor.checkCatapultTouchSensor() == true) {
+            leftMotor.setPower(-armLower);
+            rightMotor.setPower(-armLower);
+        }
+        else if (armRaise > 0) {
+            leftMotor.setPower(armRaise);
+            rightMotor.setPower(armRaise);
+
+        }
+        else {
+            leftMotor.setPower(0);
+            rightMotor.setPower(0);
+        }
+    }
+
+    public void catapultLaunch (outreachTouchSensorCatapult touchSensor) {
+        setMotorRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setMotorRunModes(currentMotorRunMode);
+        while (leftMotor.getCurrentPosition() <= LAUNCH_DISTANCE) {
+            leftMotor.setPower(LAUNCH_POWER);
+            rightMotor.setPower(LAUNCH_POWER);
+        }
+    }
+
+    public void catapultReset (outreachTouchSensorCatapult touchSensor) {
+        while (touchSensor.checkCatapultTouchSensor() == true) {
+            leftMotor.setPower(RESET_POWER);
+            rightMotor.setPower(RESET_POWER);
+        }
+    }
+
+    public void motorPowerZero () {
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+    }
+
+
+}
