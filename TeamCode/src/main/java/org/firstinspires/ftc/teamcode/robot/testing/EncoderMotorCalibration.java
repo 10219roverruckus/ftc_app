e13d62066fd3f513d9e65e5f32abf7217aed6af6
student <ftcrobotics@mtbethelchristian.org>
2019-01-09 19:26:33 -0500

    one change in a variable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/testing/EncoderMotorCalibration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/testing/EncoderMotorCalibration.java
@@ -14,7 +14,7 @@ public class EncoderMotorCalibration extends OpMode {
 
     @Override
     public void init() {
-        myEncoderMotor = hardwareMap.dcMotor.get("mineral_lift_motor");  //MAP TO YOUR RC CONFIG
+        myEncoderMotor = hardwareMap.dcMotor.get("intake_rotater_motor");  //MAP TO YOUR RC CONFIG
 //        myEncoderMotor.setDirection(DcMotorSimple.Direction.REVERSE);  // REVERSE AS NEEDED
         // Always STOP_AND_RESET_ENCODER BEFORE setting encoder mode (RUN_WITHOUT_ENCODER || RUN_USING_ENCODER || RUN_TO_POSITION).
         // Bad things await those swap the order...
